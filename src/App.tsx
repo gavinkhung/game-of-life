930ceae03265f37b7b829b03713d2ab05eef4531
Gavin Hung <gavinkhung@gmail.com>
2022-01-14 17:54:37 -0800

    Added neighbor counting

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,16 @@ interface AppProps {}
 
 const X = 20;
 const Y = 25;
+const neighboringCells:number[][] = [
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [-1, 0],
+  [1, 0],
+  [-1, -1],
+  [0, -1],
+  [-1, -1],
+];
 
 const App: FC<AppProps> = ({}) => {
   const [isActive, setIsActive] = useState<boolean>(false);
@@ -24,14 +34,45 @@ const App: FC<AppProps> = ({}) => {
     let interval: number | undefined;
     if (isActive) {
       interval = setInterval(() => {
-        // setSeconds((seconds) => seconds - 1);
+        const rows: boolean[][] = [];
+        for (let i = 0; i < Y; i++) {
+          const col: boolean[] = [];
+          for (let j = 0; j < X; j++) {
+            let neighbors = 0;
+
+            for(let k=0; k<neighboringCells.length; k++){
+              const i2:number = i+neighboringCells[k][0];
+              const j2: number = j+neighboringCells[k][1];
+              
+              if(i2>=0 && i2<X && j2>=0 && j2<Y){
+                if(grid[i2][j2]){
+                  neighbors++;
+                }
+              }
+            }
+
+            if (grid[i][j]) {
+              if (neighbors == 2 || neighbors == 3) {
+                col.push(true);
+              } else {
+                col.push(false);
+              }
+            } else {
+              if (neighbors == 3) {
+                col.push(true);
+              } else {
+                col.push(false);
+              }
+            }
+          }
+          rows.push(col);
+        }
+
+        setGrid(rows);
       }, 1000);
     }
-    // if (seconds <= 0) {
-    //   setIsActive(false);
-    // }
-    return () => clearInterval(interval);
-  }, []);
+    () => clearInterval(interval);
+  }, [isActive, grid]);
 
   const toggleCell = (i2: number, j2: number, val: boolean) => {
     const newGrid: boolean[][] = [];
