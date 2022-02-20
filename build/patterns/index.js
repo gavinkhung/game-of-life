f8ee54ee355ebc365449add6c5d90949f040b809
Gavin Hung <gavinkhung@gmail.com>
2022-02-19 20:41:21 -0800

    Removed build folder

--- build/patterns/index.js
+++
@@ -1,5 +0,0 @@
-import glider from "./glider.json.proxy.js";
-import pulse from "./pulse.json.proxy.js";
-import clear from "./clear.json.proxy.js";
-const patterns = {glider, pulse, clear};
-export default patterns;
