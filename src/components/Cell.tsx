960384635a34ddef689d95c76d6c4012714b4b97
Gavin Hung <gavinkhung@gmail.com>
2026-06-14 13:15:26 -0700

    feat: cell border

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -9,7 +9,7 @@ interface CellProps {
 
 const Cell: FC<CellProps> = ({ alive, row, col, onToggle }) => (
   <div
-    className={`border w-4 h-4 cursor-pointer ${
+    className={`border border-gray-200 w-4 h-4 cursor-pointer ${
       alive ? "bg-blue-800 opacity-50" : "bg-white"
     }`}
     onClick={() => onToggle(row, col)}
